34a4d505f18124f1354e548d6528e6c4270fb8db
Emanuel <emanuel@debmower_adj>
2017-06-26 15:18:04 +0200

    fitted CLMM to analyze the association between age difference and rel characteristics

--- Age_difference_and_rel_characteristics.R
+++ Age_difference_and_rel_characteristics.R
@@ -1,7 +1,7 @@
 library(data.table)
 library(ggplot2)
 library(dplyr)
-library(nlme)
+library(ordinal)
 
 setwd("/home/emanuel/Documents/SHIMS/shims_age_mixing")
 load("T1.agemix.Rdata")
@@ -29,6 +29,8 @@ T1.reldata <- T1.agemix %>% transmute(Uid,
                                       Money.gifts.p1,
                                       Money.gifts.p2,
                                       Money.gifts.p3)
+# code REF as NA
+T1.reldata[T1.reldata == "REF"] <- NA
 
 setDT(T1.reldata) #convert to a data.table for easy manipulation
 
@@ -40,15 +42,75 @@ DT.reldata = T1.reldata %>% melt( measure = patterns("^Age.diff", "^Condom.freq"
                                                  "Relationship.dur", "Money.gifts"),
                                   variable.name = "Partner")
 
+
 #####################################################################################
 # Removing respondents who did not report relationship 1,2,3
 
 DT.reldata.clean <- DT.reldata[!(is.na(DT.reldata$Age.difference) &
+                                   is.na(DT.reldata$Relationship.dur) &
+                                   is.na(DT.reldata$Condom.frequency) &
                                    is.na(DT.reldata$Sex.frequency) &
                                    is.na(DT.reldata$Partner.type) &
-                                   is.na(DT.reldata$Relationship.dur) &
                                    is.na(DT.reldata$Money.gifts))]
 
+DT.reldata.clean <- DT.reldata.clean %>% transmute(Uid = as.factor(Uid),
+                                                   Gender,
+                                                   Partner,
+                                                   Age.difference,
+                                                   Relationship.dur,
+                                                   Condom.frequency = as.ordered(Condom.frequency),
+                                                   Sex.frequency = as.ordered(Sex.frequency),
+                                                   Partner.type = as.ordered(Partner.type),
+                                                   Money.gifts = as.ordered(Money.gifts))
+
+DT.reldata.clean <- na.omit(DT.reldata.clean)
 #####################################################################################
 # Fitting the models
+# (a) Effect of age difference on condom frequency with participant as random effect
+
+model.a <- clmm(Condom.frequency ~ Age.difference + (1|Uid),
+                data = DT.reldata.clean,
+                link = "logit",
+                Hess = TRUE,
+                nAGQ = 7)
+
+summary(model.a)
+
+# (b) Effect of age difference on sex frequency with participant as random effect
+
+model.b <- clmm(Sex.frequency ~ Age.difference + (1|Uid),
+                data = DT.reldata.clean,
+                link = "logit",
+                Hess = TRUE,
+                nAGQ = 7)
+
+summary(model.b)
+
+# (c) Effect of age difference on partner type with participant as random effect
+
+model.c <- clmm(Partner.type ~ Age.difference + (1|Uid),
+                data = DT.reldata.clean,
+                link = "logit",
+                Hess = TRUE,
+                nAGQ = 7)
+
+summary(model.c)
+
+# (d) Effect of age difference on money gifts with participant as random effect
+
+model.d <- clmm(Money.gifts ~ Age.difference + (1|Uid),
+                data = DT.reldata.clean,
+                link = "logit",
+                Hess = TRUE,
+                nAGQ = 7)
+
+summary(model.d)
+
+
+
+
+
+
+
+
 
