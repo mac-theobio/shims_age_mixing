417dd1e382a662365d2881ca4fce612eb93ca9f2
Emanuel Muema <emanuel@aims.ac.za>
2018-02-13 11:12:25 +0200

    Update model using lme from nlme

--- Age_mixing_analysis.R
+++ Age_mixing_analysis.R
@@ -63,72 +63,55 @@ sum(table(DT.Agemix.men$Uid)>1)
 # Random intercept model
 # =======================
 agemix.model.nlme <- lme(Partner.age~ Participant.age, 
-                         data = DT.Agemix.men,method = "REML",
-                         #weights = varPower(value = 0.5, form = ~Participant.age + 1),
+                         data = DT.Agemix.men,
+                         method = "REML",
                          random = ~1|Uid)
 
 summary(agemix.model.nlme)
 
+ranef(agemix.model.nlme) # to extract random effects from the model
 
-agemix.model.1 <- lmer(Partner.age ~  Participant.age + (1|Uid),
-                       data = DT.Agemix.men)
+fixef(agemix.model.nlme) # to extract the fixed effects estimates
 
-summary(agemix.model.1)
+fitted(agemix.model.nlme) # to obtain the fitted values
 
-ranef(agemix.model.1) # to obtain the actual bj for all the groups
+AIC(agemix.model.nlme)
 
-fitted(agemix.model.1) # to obtain the fitted values
-
-AIC(agemix.model.1)
-
-coef(agemix.model.1) # to obtain the coefficient for each participant
+coef(agemix.model.nlme) # to obtain the coefficient for each participant
 
 # to fit using Maximum likelihood use update
-agemix.model.1ML <- update(agemix.model.1, REML = FALSE)
-summary(agemix.model.1ML)
-
-# assessing the variability of the parameters by profilling the fitted model
-pr01 <- profile(agemix.model.1ML)
-xyplot(pr01, aspect = 1.3, layout = c(4,1)) #profile zeta plot
-# The vertical lines in the panels delimit the 50%, 80%, 90%, 95% and 99%
-# confidence intervals derived from the test statistic.
-# To get the actual confidence intervals use confint()
-confint(pr01) 
-
-xyplot(pr01, aspect = 1.3, layout = c(4,1), absVal = TRUE) # plot of abs of zeta to visualize confidence intervals easily
-
-splom(pr01)
+agemix.model.nlmeML <- update(agemix.model.nlme, method = "ML")
+summary(agemix.model.nlmeML)
 
 library(effects)
-plot(allEffects(agemix.model.1),rug=F)
+plot(allEffects(agemix.model.nlme),rug=F)
 
-# a convenient way to plot the random eﬀects with 95% conﬁdence intervals along with the estimated random eﬀects.
-dotplot(ranef(agemix.model.1, condVar=T),
-        ylab = "Participant ID",
-        scales = list(y = list(draw = FALSE)))
-#or
-qqmath(ranef(agemix.model.1, condVar=TRUE))
+# # a convenient way to plot the random eﬀects using lmer with 95% conﬁdence intervals along with the estimated random eﬀects.
+# dotplot(ranef(agemix.model.1, condVar=T),
+#         ylab = "Participant ID",
+#         scales = list(y = list(draw = FALSE)))
 
 ggplot(data = DT.Agemix.men, aes(x = Participant.age, y = Partner.age)) +
   geom_point()+
   theme_set(theme_bw()) +
-  geom_abline(intercept = fixef(agemix.model.1)[1], slope = fixef(agemix.model.1)[2], col = "red")+
-  geom_abline(intercept = coef(agemix.model.1)$Uid["0000208",]$`(Intercept)`,
-              slope = coef(agemix.model.1)$Uid["0000208",]$Participant.age, 
+  geom_abline(intercept = fixef(agemix.model.nlme)[1], slope = fixef(agemix.model.nlme)[2], col = "red")+
+  geom_abline(intercept = coef(agemix.model.nlme)["0000208",]$`(Intercept)`,
+              slope = coef(agemix.model.nlme)["0000208",]$Participant.age, 
               col = "blue") +
-  geom_abline(intercept = coef(agemix.model.1)$Uid["0012036",]$`(Intercept)`,
-              slope = coef(agemix.model.1)$Uid["0012036",]$Participant.age, 
+  geom_abline(intercept = coef(agemix.model.nlme)["0012036",]$`(Intercept)`,
+              slope = coef(agemix.model.nlme)["0012036",]$Participant.age, 
               col = "green")
 
+agemix.model.2nlme <- lme(Partner.age~ Participant.age + Partner.type, 
+                          data = DT.Agemix.men,
+                          method = "REML",
+                          random = ~1|Uid)
 
-agemix.model.2 <- lmer(Partner.age ~ Participant.age + Partner.type + (1|Uid),
-                     data = DT.Agemix.men)
+summary(agemix.model.2nlme)
 
-summary(agemix.model.2)
+agemix.model.2nlmeML <- update(agemix.model.2nlme, method = "ML")
 
-agemix.model.2ML <- update(agemix.model.2, REML = FALSE)
-
-anova(agemix.model.1ML, agemix.model.2ML)
+anova(agemix.model.nlmeML, agemix.model.2nlmeML)
 # adding partner type does not improve the model
 
 # ==================
