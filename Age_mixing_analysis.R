7f96e0c7cc818bc3471fb89813ecc57318ce3b47
Emanuel Muema <emanuel@aims.ac.za>
2018-02-20 15:18:57 +0200

    code cleaning

--- Age_mixing_analysis.R
+++ Age_mixing_analysis.R
@@ -71,57 +71,67 @@ sum(table(DT.Agemix.men$Uid)>1)
 # Random intercept model
 # =======================
 
-# =======
-# Step 1
-# =======
-agemix.model.nlme.null <- lme(Partner.age ~ 1, 
+# ================================
+# Step 1, model with no covariates
+# ================================
+agemix.M0 <- lme(Partner.age ~ 1, 
                               data = DT.Agemix.men,
                               method = "REML",
                               random = ~1|Uid)
 
-summary(agemix.model.nlme.null)
+summary(agemix.M0)
 # null model helps us understand the structure of the data. Gives baseline AIC/BIC values
 # ICC = 0.19 which means that the correlation of partner age score within an individual is 0.19
 
 
 # fit a marginal model using gls
-agemix.model.gls <- gls(Partner.age ~ 1, 
+agemix.M0.gls <- gls(Partner.age ~ 1, 
                         data = DT.Agemix.men)
 
-summary(agemix.model.gls)
+summary(agemix.M0.gls)
 
 # do a likelihood ratio test 
-anova(agemix.model.gls, agemix.model.nlme.null)
+anova(agemix.M0.gls, agemix.M0)
 # the result suggests that random participant effect should be retained (P < 0.05)
 
-# =======
-# Step 2
-# =======
-agemix.model.nlme <- lme(Partner.age ~ Participant.age, 
+# ===================================
+# Step 2, adding level one covariate
+# ===================================
+agemix.M1 <- lme(Partner.age ~  Partner.type, 
                          data = DT.Agemix.men,
                          method = "REML",
                          random = ~1|Uid)
 
-summary(agemix.model.nlme)
+summary(agemix.M1)
 
-anova(update(agemix.model.nlme.null, method= "ML"),update(agemix.model.nlme, method= "ML"))
+anova(update(agemix.M0, method= "ML"),update(agemix.M1, method= "ML"))
+# not significant so we keep our null model
 
-ranef(agemix.model.nlme) # to extract random effects from the model
+# =================================
+# Step 3, adding level 2 covariate
+# =================================
 
-fixef(agemix.model.nlme) # to extract the fixed effects estimates
+agemix.M2 <- lme(Partner.age ~  Participant.age, 
+                         data = DT.Agemix.men,
+                         method = "REML",
+                         random = ~1|Uid)
 
-fitted(agemix.model.nlme) # to obtain the fitted values
+summary(agemix.M2)
 
-AIC(agemix.model.nlme)
+ranef(agemix.M2) # to extract random effects from the model
 
-coef(agemix.model.nlme) # to obtain the coefficient for each participant
+fixef(agemix.M2) # to extract the fixed effects estimates
 
-# to fit using Maximum likelihood use update
-agemix.model.nlmeML <- update(agemix.model.nlme, method = "ML")
-summary(agemix.model.nlmeML)
+fitted(agemix.M2) # to obtain the fitted values
+
+coef(agemix.M2) # to obtain the coefficient for each participant
+
+# to fit using Maximum likelihood use update and do a likelihood ratio test 
+anova(update(agemix.M0, method= "ML"), update(agemix.M2, method= "ML"))
+# the result suggests that the new model should be retained (P < 0.0001)
 
 library(effects)
-plot(allEffects(agemix.model.nlme),rug=F)
+plot(allEffects(agemix.M2),rug=F)
 
 # # a convenient way to plot the random eﬀects using lmer with 95% conﬁdence intervals along with the estimated random eﬀects.
 # dotplot(ranef(agemix.model.1, condVar=T),
@@ -132,32 +142,32 @@ ggplot(DT.Agemix.men,aes(Participant.age,Partner.age)) +
   geom_jitter(size=3,color="black", width = 0.25, height = 0.25, alpha = 0.5) +
   xlab("Age") +
   ylab("Partner age") + 
-  geom_abline(intercept = fixef(agemix.model.nlme)[["(Intercept)"]], 
-              slope = fixef(agemix.model.nlme)[["Participant.age"]], 
+  geom_abline(intercept = fixef(agemix.M2)[["(Intercept)"]], 
+              slope = fixef(agemix.M2)[["Participant.age"]], 
               col = "red",
               size = 1.25) +
-  geom_abline(intercept = coef(agemix.model.nlme)["0000208",]$`(Intercept)`,
-              slope = coef(agemix.model.nlme)["0000208",]$Participant.age, 
+  geom_abline(intercept = coef(agemix.M2)["0000208",]$`(Intercept)`,
+              slope = coef(agemix.M2)["0000208",]$Participant.age, 
               col = "blue",
               size = 1.25) +
-  geom_abline(intercept = coef(agemix.model.nlme)["0012036",]$`(Intercept)`,
-              slope = coef(agemix.model.nlme)["0012036",]$Participant.age, 
+  geom_abline(intercept = coef(agemix.M2)["0012036",]$`(Intercept)`,
+              slope = coef(agemix.M2)["0012036",]$Participant.age, 
               col = "green",
               size = 1.25) +
   scale_x_continuous(labels = function(x)x+15, breaks = scales::pretty_breaks(n = 10)) +
   scale_y_continuous(breaks = scales::pretty_breaks(n = 10))
 
-agemix.model.2nlme <- lme(Partner.age~ Participant.age + Partner.type, 
-                          data = DT.Agemix.men,
-                          method = "REML",
-                          random = ~1|Uid)
-
-summary(agemix.model.2nlme)
-
-agemix.model.2nlmeML <- update(agemix.model.2nlme, method = "ML")
-
-anova(agemix.model.nlmeML, agemix.model.2nlmeML)
-# adding partner type does not improve the model
+# agemix.M3 <- lme(Partner.age~ Participant.age + Partner.type, 
+#                           data = DT.Agemix.men,
+#                           method = "REML",
+#                           random = ~1|Uid)
+# 
+# summary(agemix.M3)
+# 
+# agemix.M3ML <- update(agemix.M3, method = "ML")
+# 
+# anova(agemix.M2ML, agemix.M3ML)
+# # adding partner type does not improve the model
 
 
 # ==================
@@ -166,66 +176,82 @@ anova(agemix.model.nlmeML, agemix.model.2nlmeML)
 
 # heteroscedastic errors in nlme
 # plotting residuals against the fitted values - detecting heteroscedasticity
-plot(agemix.model.nlme, residuals(.) ~ fitted(.), abline = 0 )
+
+png("detecthetero.png", 
+    width = 6.25,
+    height = 5.25,
+    units = "in",
+    res = 1200, 
+    pointsize = 4)
+plot(agemix.M2, residuals(.) ~ fitted(.), abline = 0 )
+dev.off()
 
 # check why there is heteroscedasticity
-plot(agemix.model.nlme, residuals(.) ~ Participant.age, abline = 0)
+
+png("detecthetero1.png", 
+    width = 6.25,
+    height = 5.25,
+    units = "in",
+    res = 1200, 
+    pointsize = 4)
+plot(agemix.M2, residuals(.) ~ Participant.age, abline = 0)
+dev.off()
+
 # confirms that the within group variability increases with participant age 
 # var(eij) increases when participant age increases
 
-agemix.model.nlme.hetero <- lme(Partner.age~ Participant.age, 
+agemix.M4 <- lme(Partner.age~ Participant.age, 
                                  data = DT.Agemix.men,method = "REML",
                                  weights = varPower(value = 0, # this starting point is the homoscedastic form
                                                     form = ~Participant.age),
                                  random = ~1|Uid)
 
-summary(agemix.model.nlme.hetero)
+summary(agemix.M4)
 
 # compare the homoscedastic(constant variance) and heteroscedastic models
-anova(agemix.model.nlme,agemix.model.nlme.hetero)
+anova(agemix.M2,agemix.M4)
 # the result of this likelihood ratio test is significant (p<0.001), we choose the 
 # heteroscedastic model as our preferred model.
 
 #=====
 # proving how the variance function is computed. This shows if you are older the variance is 
 # much higher 
-
-DT.Agemix.men.new <- DT.Agemix.men
-DT.Agemix.men.new$varweight = 1/abs(DT.Agemix.men$Participant.age)^0.6198723 
-
+# 
+# DT.Agemix.men.new <- DT.Agemix.men
+# DT.Agemix.men.new$varweight = 1/abs(DT.Agemix.men$Participant.age)^0.6198723 
+#
 #=====
 
-agemix.model.nlme.hetero1 <- lme(Partner.age~ Participant.age, 
+agemix.M5 <- lme(Partner.age~ Participant.age, 
                                  data = DT.Agemix.men,method = "REML",
                                  weights = varPower(value = 0, # this starting point is the homoscedastic form
                                                     form = ~ Participant.age + 1),
                                  random = ~1|Uid)
 
-summary(agemix.model.nlme.hetero1)
+summary(agemix.M5)
 
 # compare the heteroscedastic models
-AIC(agemix.model.nlme.hetero,agemix.model.nlme.hetero1)
-# new model, agemix.model.nlme.hetero1, has a much smaller AIC/BIC and so we choose it as our preferred
+AIC(agemix.M4,agemix.M5)
+# new model, agemix.M5, has a much smaller AIC/BIC and so we choose it as our preferred
 # model
  
-agemix.model.nlme.hetero2 <- lme(Partner.age~ Participant.age, 
+agemix.M6 <- lme(Partner.age~ Participant.age, 
                             data = DT.Agemix.men,method = "REML",
                             weights = varConstPower(form = ~Participant.age, fixed = list(const =1)),
                             random = ~1|Uid)
 
-summary(agemix.model.nlme.hetero2)
+summary(agemix.M6)
 
 # compare the heteroscedastic models
-AIC(agemix.model.nlme.hetero1,agemix.model.nlme.hetero2)
+AIC(agemix.M5,agemix.M6)
 # these two models have the same AIC so we retain our old model as our preferred model.
 
 # plot the standardized residuals shows a reasonably homogeneous pattern of the variability
 # of the residuals
 
-plot(agemix.model.nlme.hetero, resid(., type = "normalized") ~ Participant.age, abline = 0)
-plot(agemix.model.nlme.hetero1, resid(., type = "normalized") ~ Participant.age, abline = 0)
+plot(agemix.M5, resid(., type = "normalized") ~ Participant.age, abline = 0)
 
 # assess the variability of the variance parameter estimate
-intervals(agemix.model.nlme.hetero1)
+intervals(agemix.M5)
 
 
