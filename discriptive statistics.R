ed2f712747ede421a6d98cbf7a4ae6074b46b2df
Emanuel Muema <emanuel@aims.ac.za>
2018-05-10 10:38:47 +0200

    Script with general discriptive statistics

---
+++ discriptive statistics.R
@@ -0,0 +1,12 @@
+# General Discriptive Statistics ------------------------------------------
+
+# load libraries
+
+library(tidyverse)
+
+## load data and functions
+
+load("/Users/emanuel/Dropbox/SHIMS Baseline data/DT.Agemix.men.Rdata") # full dataset
+theme_set(theme_bw()) # set global plot theme 
+source("Functions_for_SHIMS_study.R")
+
